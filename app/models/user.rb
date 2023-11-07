c3033e59c537e12748402f4699a249fd7b065208
Isaac Andriamiharisoa <nyainaisaac@gmail.com>
2023-11-07 12:42:31 +0400

    cloudinary init

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,10 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  has_one_attached :photo
+
+  def upload_image_to_cloudinary
+    raise
+    Cloudinary::Uploader.upload(photo.path, public_id: username)
+  end
 end
