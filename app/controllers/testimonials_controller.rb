d83bec3c444591e67677d324438947ca2516a0eb
Isaac Andriamiharisoa <nyainaisaac@gmail.com>
2023-11-16 11:23:44 +0400

    added new testimonial front

--- app/controllers/testimonials_controller.rb
+++ app/controllers/testimonials_controller.rb
@@ -6,7 +6,7 @@ class TestimonialsController < ApplicationController
   def create
     @testimonial = Testimonial.new(testimonial_params)
     if @testimonial.save
-      redirect_to root_path
+      redirect_to root_path, notice: "Your testimonial is published"
     else
       render :new, status: :unprocessable_entity
     end
