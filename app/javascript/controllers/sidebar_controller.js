3144affbdcf45a6aa692b763d95ef21ba2d7486e
Isaac Andriamiharisoa <nyainaisaac@gmail.com>
2026-05-30 13:27:20 +0400

    feat: add sidebar Stimulus controller

---
+++ app/javascript/controllers/sidebar_controller.js
@@ -0,0 +1,17 @@
+import { Controller } from "@hotwired/stimulus"
+
+export default class extends Controller {
+  static targets = ["sidebar", "overlay"]
+
+  open() {
+    this.sidebarTarget.classList.add("app-navbar__sidebar--open")
+    this.overlayTarget.classList.add("app-navbar__overlay--visible")
+    document.body.style.overflow = "hidden"
+  }
+
+  close() {
+    this.sidebarTarget.classList.remove("app-navbar__sidebar--open")
+    this.overlayTarget.classList.remove("app-navbar__overlay--visible")
+    document.body.style.overflow = ""
+  }
+}
